743b9b54dee8e45c94ae33f85ddeb87355498946
Bo <openaphid@gmail.com>
2020-11-05 22:17:38 +0800

    cleaner impl of timsort in go1.x
    pkg: github.com/openaphid/dualpivotsort/internal/sort_slice_tim
    BenchmarkVSSort/TimSort-256-8         	   20600	     57366 ns/op	   14672 B/op	     262 allocs/op
    BenchmarkVSSort/TimSort-ManualConvert-256-8         	   28944	     41001 ns/op	   14640 B/op	     261 allocs/op
    BenchmarkVSSort/BuiltinSort-256-8                   	   27214	     43817 ns/op	     112 B/op	       3 allocs/op
    BenchmarkVSSort/TimSort-1024-8                      	    4201	    279245 ns/op	   61776 B/op	    1031 allocs/op
    BenchmarkVSSort/TimSort-ManualConvert-1024-8        	    4934	    229771 ns/op	   61744 B/op	    1030 allocs/op
    BenchmarkVSSort/BuiltinSort-1024-8                  	    5995	    210020 ns/op	     112 B/op	       3 allocs/op
    BenchmarkVSSort/TimSort-4192-8                      	    1071	   1088129 ns/op	  269872 B/op	    4201 allocs/op
    BenchmarkVSSort/TimSort-ManualConvert-4192-8        	    1309	    915721 ns/op	  269840 B/op	    4200 allocs/op
    BenchmarkVSSort/BuiltinSort-4192-8                  	    1386	    832606 ns/op	     112 B/op	       3 allocs/op
    BenchmarkVSSort/TimSort-16768-8                     	     244	   4887797 ns/op	 1065521 B/op	   16779 allocs/op
    BenchmarkVSSort/TimSort-ManualConvert-16768-8       	     289	   4195873 ns/op	 1065489 B/op	   16778 allocs/op
    BenchmarkVSSort/BuiltinSort-16768-8                 	     326	   3924185 ns/op	     112 B/op	       3 allocs/op

--- internal/sort_slice_tim/sort_slice_tim_test.go
+++ internal/sort_slice_tim/sort_slice_tim_test.go
@@ -17,101 +17,6 @@ func (p Person) String() string {
 	return fmt.Sprintf("Person(%d, %s)", p.age, p.name)
 }
 
-type PersonSlice []Person
-
-func (p PersonSlice) cmp(compare CompareFunc, i, j int) int {
-	return compare(p[i], p[j])
-}
-
-var _ SliceInterface = (*PersonSlice)(nil)
-
-func (p PersonSlice) make(n int) SliceInterface {
-	return make(PersonSlice, n)
-}
-
-func (p PersonSlice) copy(src SliceInterface) {
-	copy(p, src.(PersonSlice))
-}
-
-func (p PersonSlice) get(i int) interface{} {
-	return p[i]
-}
-
-func (p PersonSlice) set(i int, v interface{}) {
-	p[i] = v.(Person)
-}
-
-func (p PersonSlice) swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
-}
-
-func (p PersonSlice) len() int {
-	return len(p)
-}
-
-func (p PersonSlice) slice(i int) SliceInterface {
-	return p[i:]
-}
-
-func (p PersonSlice) slice2(i, j int) SliceInterface {
-	return p[i:j]
-}
-
-type PersonSliceWithStat []Person
-
-var invokeCount = make(map[string]int)
-
-func resetCount() {
-	invokeCount = make(map[string]int)
-}
-
-func (p PersonSliceWithStat) cmp(compare CompareFunc, i, j int) int {
-	invokeCount["cmp"]++
-	return compare(p[i], p[j])
-}
-
-var _ SliceInterface = (*PersonSliceWithStat)(nil)
-
-func (p PersonSliceWithStat) make(n int) SliceInterface {
-	invokeCount["make"]++
-	return make(PersonSliceWithStat, n)
-}
-
-func (p PersonSliceWithStat) copy(src SliceInterface) {
-	invokeCount["copy"]++
-	copy(p, src.(PersonSliceWithStat))
-}
-
-func (p PersonSliceWithStat) get(i int) interface{} {
-	invokeCount["get"]++
-	return p[i]
-}
-
-func (p PersonSliceWithStat) set(i int, v interface{}) {
-	invokeCount["set"]++
-	p[i] = v.(Person)
-}
-
-func (p PersonSliceWithStat) swap(i, j int) {
-	invokeCount["swap"]++
-	p[i], p[j] = p[j], p[i]
-}
-
-func (p PersonSliceWithStat) len() int {
-	invokeCount["len"]++
-	return len(p)
-}
-
-func (p PersonSliceWithStat) slice(i int) SliceInterface {
-	invokeCount["slice"]++
-	return p[i:]
-}
-
-func (p PersonSliceWithStat) slice2(i, j int) SliceInterface {
-	invokeCount["slice2"]++
-	return p[i:j]
-}
-
 func prepare(a []Person) {
 	for i, _ := range a {
 		a[i] = Person{
@@ -125,14 +30,9 @@ var byAge = func(o1, o2 interface{}) int {
 	return o1.(Person).age - o2.(Person).age
 }
 
-var byAgeWithStat = func(o1, o2 interface{}) int {
-	invokeCount["byAge"]++
-	return o1.(Person).age - o2.(Person).age
-}
-
 func TestByAge(t *testing.T) {
 	for i := 1; i <= 1024*10; i++ {
-		persons := make(PersonSlice, i)
+		persons := make([]Person, i)
 		prepare(persons)
 
 		Sort(persons, byAge)
@@ -145,65 +45,17 @@ func TestByAge(t *testing.T) {
 	}
 }
 
-type PersonBuiltinSlice []Person
-
-var _ builtinsort.Interface = (*PersonBuiltinSlice)(nil)
-
-func (p PersonBuiltinSlice) Len() int {
-	invokeCount["Len"]++
-	return len(p)
-}
-
-func (p PersonBuiltinSlice) Less(i, j int) bool {
-	invokeCount["Less"]++
-	return p[i].age < p[j].age
-}
-
-func (p PersonBuiltinSlice) Swap(i, j int) {
-	invokeCount["Swap"]++
-	p[i], p[j] = p[j], p[i]
-}
-
-const invokeDataSize = 1024
-
-func TestInvokeCount(t *testing.T) {
-	persons := make([]Person, invokeDataSize)
-	prepare(persons)
-
-	{
-		resetCount()
-		persons2 := make(PersonSliceWithStat, invokeDataSize)
-		for i, _ := range persons {
-			persons2[i] = persons[i]
-		}
-
-		Sort(persons2, byAgeWithStat)
-		log.Printf("tim invoke count: %v", invokeCount)
-	}
-
-	{
-		resetCount()
-		persons2 := make(PersonBuiltinSlice, invokeDataSize)
-		for i, _ := range persons {
-			persons2[i] = persons[i]
-		}
-
-		builtinsort.Sort(persons2)
-		log.Printf("builtin invoke count: %v", invokeCount)
-	}
-}
-
 var benchmarkSizes = []int{256, 1024, 4192, 16768}
 
 func BenchmarkVSSort(t *testing.B) {
 	for _, size := range benchmarkSizes {
-		var data = make(PersonSlice, size)
+		var data = make([]Person, size)
 		prepare(data)
 
 		t.Run(fmt.Sprintf("TimSort-%d", size), func(t *testing.B) {
 			for i := 0; i < t.N; i++ {
 				t.StopTimer()
-				dup := make(PersonSlice, size)
+				dup := make([]Person, size)
 				copy(dup, data)
 				t.StartTimer()
 				Sort(dup, func(o1, o2 interface{}) int {
@@ -212,10 +64,32 @@ func BenchmarkVSSort(t *testing.B) {
 			}
 		})
 
+		t.Run(fmt.Sprintf("TimSort-ManualConvert-%d", size), func(t *testing.B) {
+			for i := 0; i < t.N; i++ {
+				t.StopTimer()
+				dup := make([]Person, size)
+				copy(dup, data)
+				t.StartTimer()
+
+				convert := make([]interface{}, size)
+				for i, _ := range convert {
+					convert[i] = dup[i]
+				}
+
+				SortInterfaces(convert, func(o1, o2 interface{}) int {
+					return o1.(Person).age - o2.(Person).age
+				})
+
+				for i, _ := range dup {
+					dup[i] = convert[i].(Person)
+				}
+			}
+		})
+
 		t.Run(fmt.Sprintf("BuiltinSort-%d", size), func(t *testing.B) {
 			for i := 0; i < t.N; i++ {
 				t.StopTimer()
-				dup := make(PersonSlice, size)
+				dup := make([]Person, size)
 				copy(dup, data)
 				t.StartTimer()
 				builtinsort.Slice(dup, func(i, j int) bool {
